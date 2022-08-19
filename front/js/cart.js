901ed3053f359bb22308aa3ed0778b89510c335c
Guillaume Dorce <polynux.dev@gmail.com>
2022-08-19 10:03:36 +0200

    fix page redirection

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-const url = "http://grossebeut.eu:3000/api";
+const url = "http://localhost:3000/api";
 
 class Cart {
   constructor() {
@@ -232,7 +232,7 @@ function submitForm(cart) {
       return res.json();
     })
     .then(res => {
-      location.href = "/confirmation?orderId=" + res.orderId;
+      location.href = "/confirmation.html?orderId=" + res.orderId;
     })
     .catch(alert);
 }
